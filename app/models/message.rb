8970e8de11e502c6f6fb341826ea9ce7d8f8c77c
Jonathan Porta <jonathan@jonathanporta.com>
2014-08-24 20:13:39 -0700

    Playing with callbacks in models.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
+
+  after_create :create_activity_entries
+
   def self.sent_to(user)
     where recipient_id: user.id
   end
@@ -6,4 +9,9 @@ class Message < ActiveRecord::Base
   def self.sent_by(user)
     where sender_id: user.id
   end
+
+  private
+    def create_activity_entries
+      # TODO Create activities for each user.
+    end
 end
