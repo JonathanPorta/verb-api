1c9430ca45a523e06b8912b3f6bcd5a4547734cb
Jonathan Porta <jonathan@jonathanporta.com>
2014-08-27 00:02:42 -0600

    Create activities when a message is sent.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,9 @@
 class Activity < ActiveRecord::Base
+
+  def self.activities_for_message(message)
+    # Sender activity
+    Activity.create user_id: message.sender_id, message_id: message.id, body: "You sent: #{ message.body }"
+    # Recipient activity
+    Activity.create user_id: message.recipient_id, message_id: message.id, body: "You received: #{ message.body }"
+  end
 end
