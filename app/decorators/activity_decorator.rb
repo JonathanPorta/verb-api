2ada8ccf45cf0a4d653d621436ff01abd2443fb4
Jonathan Porta <jonathan@jonathanporta.com>
2014-09-28 03:12:09 -0700

    add a naive message build to the activity decorator

--- app/decorators/activity_decorator.rb
+++ app/decorators/activity_decorator.rb
@@ -3,4 +3,39 @@ class ActivityDecorator < Draper::Decorator
 
   decorates_association :user
   decorates_association :message
+
+  def activity_message
+    # hashtag uhg.
+    if object.type == 'sent'
+      if message.acknowledged_at
+        acknowledged_message_for_sender
+      else
+        unacknowledged_message_for_sender
+      end
+    elsif object.type == 'received'
+      if message.acknowledged_at
+        acknowledged_message_for_recipient
+      else
+        unacknowledged_message_for_recipient
+      end
+    end
+  end
+
+  private
+
+  def acknowledged_message_for_sender
+    "You did #{ message.verb } #{ message.recipient.first_name }!"
+  end
+
+  def acknowledged_message_for_recipient
+    "#{ message.sender.first_name } did #{ message.verb } you!"
+  end
+
+  def unacknowledged_message_for_sender
+    "You tried to #{ message.verb } #{ message.recipient.first_name }!"
+  end
+
+  def unacknowledged_message_for_recipient
+    "#{ message.sender.first_name } wants to #{ message.verb } you!"
+  end
 end
--- app/views/activities/index.json.jbuilder
+++ app/views/activities/index.json.jbuilder
@@ -1,6 +1,6 @@
 json.array!(@activities) do |activity|
   activity = activity.decorate
-  json.extract! activity, :id, :type
+  json.extract! activity, :id, :type, :activity_message
   json.message do
     # Sadly, this step is needed because extract doesn't find the at_in_words methods from the decorator.
     json.extract! activity.message, :id, :verb, :sender, :recipient, :acknowledged_at, :acknowledged_at_in_words, :created_at, :created_at_in_words
