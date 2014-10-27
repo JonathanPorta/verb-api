9d83e4676e7c90e6acad63935674c93aadc289fa
Jonathan Porta <jonathan@jonathanporta.com>
2014-10-27 07:15:45 -0700

    Retry installing ruby gems

--- spec/controllers/friends_controller_spec.rb
+++ spec/controllers/friends_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe FriendsController, :type => :controller do
+  before :each do
+    @sender = FactoryGirl.create :user
+
+    request.session[:user_id] = @sender.id
+    # TODO: Make some peoples message each other.
+  end
 
   describe "GET index.json" do
     it "returns http success" do
